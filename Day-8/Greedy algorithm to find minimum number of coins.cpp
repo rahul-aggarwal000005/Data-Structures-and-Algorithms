6d08955f625d6857c6c948856042993e55118dbe
Rahul Aggarwal <rahul.aggarwal000005@gmail.com>
2022-05-12 08:43:37 +0530

    Day-8

--- Day-8/Greedy algorithm to find minimum number of coins.cpp
+++ Day-8/Greedy algorithm to find minimum number of coins.cpp
@@ -1,37 +1,14 @@
-// Greedy Appraoch
-int coinChange(vector<int>& coins, int amount) {
-    sort(coins.begin(),coins.end());
-    int ans = 0;
-    for(int i=coins.size()-1;i>=0;i--){
-        int total = amount / coins[i];
-        int amt = total * coins[i];
-        ans += total;
-        amount -= amt;
-    }
-    if(amount != 0) return -1;
-    return ans;
+int findMinimumCoins(int amount) 
+{
+    // Write your code here
+	vector<int> arr = {1, 2, 5, 10, 20, 50, 100, 500, 1000};
+	reverse(arr.begin(),arr.end());
+	int ans = 0;
+	for(int i=0;i<arr.size();i++){
+		if(amount < arr[i]) continue;
+		int cnt = amount / arr[i];
+		amount -= cnt * arr[i];
+		ans += cnt;
+	}
+	return ans;
 }
-
-// DP approach
-class Solution {
-public:
-    long long dp[20000];
-    long long helper(vector<int> &a,int amt){
-        
-        if(amt == 0) return 0ll;
-        if(dp[amt] != -1) return dp[amt];
-        long long res = INT_MAX;
-        for(auto x:a){
-            if(amt >= x){
-                res = min(res,1ll + helper(a,amt-x));
-            }
-        }
-        return dp[amt] = res;
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        memset(dp,-1,sizeof(dp));
-        long long val = helper(coins,amount);
-        if(val >= INT_MAX) return -1;
-        return val;
-    }
-};
